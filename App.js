3ff6935c05296bf730dc056e030033facc05aaa0
vicky1104 <vicky114chauhan@gmail.com>
2022-06-10 16:49:51 +0530

    new project

---
+++ App.js
@@ -0,0 +1,56 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Dimensions,
+} from 'react-native';
+
+const App = () => {
+  console.log(Dimensions.get('screen').width);
+
+  return (
+    <Text
+      style={styles.sectionContainer}
+      onPress={() =>
+        console.trace(
+          'fhdsg......................................................................................usid',
+        )
+      }>
+      ghd
+    </Text>
+  );
+};
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
